672939de0355b80af6102343cda6af2a342a1634
Madhuri313 <udathumadhuri@gmail.com>
2025-09-06 20:38:37 +0530

    Added environ variables

--- email_campaign_project/email_campaign_application/models.py
+++ email_campaign_project/email_campaign_application/models.py
@@ -10,14 +10,13 @@ class Subscriber(models.Model):
 
 
 def __str__(self):
-    return f"{self.email} ({'active' if self.is_active else 'inactive'})"
+    return f"{self.email} ({'Active' if self.is_active else 'Inactive'})"
 
 
 class Campaign(models.Model):
     subject = models.CharField(max_length=255)
     preview_text = models.CharField(max_length=512, blank=True)
     article_url = models.URLField(blank=True)
-    html_content = models.TextField(blank=True)
     plain_text_content = models.TextField(blank=True)
     published_date = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
--- email_campaign_project/email_campaign_project/settings.py
+++ email_campaign_project/email_campaign_project/settings.py
@@ -9,15 +9,15 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-#import environ
-#env=environ.Env()
-#environ.Env.read_env()
+import environ
+env=environ.Env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -87,8 +87,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'users',
-        'USER': 'root',
-        'PASSWORD': 'Madhuri@2003',
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
         'HOST': 'localhost',
         'PORT': '3306'
     }
